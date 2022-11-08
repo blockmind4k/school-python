5ef9ab4d6e77622df124be129a323a8dd7e205dd
Blockmind4k <blockmind4k@gmail.com>
2022-11-08 23:22:38 +0530

    Added Comments and some files

--- IPP/school-project.py
+++ IPP/school-project.py
@@ -38,21 +38,21 @@ def merge_sort(num_array):
         while i < len(left_num_array) and j < len(right_num_array):
             # comparing left most indices of the split arrays...
             if left_num_array[i] < right_num_array[j]:
-                num_array[k] = left_num_array[i]
+                num_array[k] = left_num_array[i] # adding left most index from the left array to merged list
                 i = i + 1
                 k = k + 1
             
             else:
-                num_array[k] = right_num_array[j]
+                num_array[k] = right_num_array[j] # adding right most index from the right array to merged list
                 j = j + 1
                 k = k + 1
         
-        while i < len(left_num_array):
+        while i < len(left_num_array): # if and when left most index has not reached end of left array 
             num_array[k] = left_num_array[i]
             k = k + 1
             i = i + 1
 
-        while j < len(right_num_array):
+        while j < len(right_num_array): # if and when right most index has not reached end of right array
             num_array[k] = right_num_array[j]
             k = k + 1
             j = j + 1
@@ -60,24 +60,23 @@ def merge_sort(num_array):
 
 #   taking input from a file and converting it into a list
 def file_input():
-    data = open('tests.txt', 'r')
-    inputData = data.read()
+    data = open('input.txt', 'r')
+    inputData = data.read() # reads input.txt
 
-    dataList = inputData.split('\n')
+    dataList = inputData.split('\n') # splits data from input.txt and converts it to a list
 
-
-    for i in range(0, len(dataList), 1):
-        dataList[i] = float(dataList[i])
-
-    print(dataList)
-
-    merge_sort(dataList)
-    # print(dataList)
-
-    outputData = open("tests-result.txt", "w")
 
     for i in range(0, len(dataList), 1):
-        outputData.write(str(dataList[i]))
+        dataList[i] = float(dataList[i]) # projecting string into a float
+
+    #print(dataList)
+
+    merge_sort(dataList) # sorts the list into (ascending order)
+
+    outputData = open("output.txt", "w") # opens the output file
+
+    for i in range(0, len(dataList), 1): # iterates for every element in list
+        outputData.write(str(dataList[i])) # writes each element from the list onto the output file
         outputData.write('\n')
 
 
@@ -97,13 +96,13 @@ def func_numlist(): #numbers ascending sort
 
 #   reversing a sorted list
 def func_descending():
-    var_num_ls = []
-    var_n = int(input('Enter number of elements: '))
-    for i in range (0, var_n):
-        var_num_elmt = float(input('Enter a number: '))
-        var_num_ls.append(var_num_elmt)
+    var_num_ls = [] # declaring empty list
+    var_n = int(input('Enter number of elements: ')) # taking number of elements
+    for i in range (0, var_n): # declaring for loop
+        var_num_elmt = float(input('Enter a number: ')) # number input
+        var_num_ls.append(var_num_elmt) # appending input number to a list
 
-    merge_sort(var_num_ls)
+    merge_sort(var_num_ls) # sorting the list in ascending order...
 
     arr_len = len(var_num_ls)
 
@@ -125,18 +124,19 @@ def func_alphalist(): #array of strings sort
     var_n_strings = int(input('Enter number of elements: '))
     for i in range (0, var_n_strings):
         var_alph_elmt = input('Enter a string: ')
-        var_alph_ls.append(var_alph_elmt)
+        var_alph_ls.append(var_alph_elmt) # appends element to list
     
     print('The array of strings is: ', var_alph_ls, '\n')
-    var_alph_ls.sort()
+    var_alph_ls.sort() # sorts alphabet list
     print ('The Alphabetically sorted list is: ', var_alph_ls)
 
 var_usr_0 = 0
 
+# running an infinite loop unitl user decides to break 
 while True:
     if var_usr_0 == 0:
-        text_banner()
-        print('----------MENU----------\n')
+        text_banner() # calling the text_banner so that logo is printed
+        print('\n----------MENU----------\n')
 
         print('1. Sort a list')
         print('2. Sort a List of Names according to Alphabetic order')
@@ -146,23 +146,26 @@ while True:
         
         
         if (var_usr_1 == 1):
+            # numeric sorting
             print('Do you want to sort input from a text file? If so, enter "file" \n')
             print('\n If you want to input data manually, enter "manual"\n')
             choice = input('Enter input: ')
             choice.lower
             if(choice == "file"):
                 print('\n___________________________________________________________\n')
-                print('\nNote: Please save input.txt before running this program')
+                print('Note: Please save input.txt before running this program')
                 print('\n___________________________________________________________\n')
-                file_input()
-                print("Output is stored in output.txt")
+
+                file_input() # calling file_input to read input from a file
+
+                print("Output is stored in output.txt\n")
                 print('Enter "menu" to go back to menu')
                 menu = input('Input here: ')
                 menu.lower()
                 if(menu == "menu"):
-                    var_usr_0 = 0
+                    var_usr_0 = 0 # redirects to menu
 
-            if(choice == "manual" or var_usr_0 == 2):
+            if(choice == "manual" or var_usr_0 == 2): # manual
                 print('A. Sort in Ascending order')
                 print('D. Sort in Descending order')
                 print('Enter "menu" to go back to menu')
@@ -175,19 +178,16 @@ while True:
                     func_descending()
 
                 elif(var_usr_2 == "menu"):
-                    var_usr_0 = 0
+                    var_usr_0 = 0 # redirects to menu
 
                 else:
                     print('Enter a valid input')
-                    var_usr_0 = 2
-
+                    var_usr_0 = 2 # redirects to manual
             
-        else:
-            print('\nInput a proper value...\n')
-            var_usr_0 = 0
+            else:
+                var_usr_1 = 1 # redirects to start of numeric sorting
 
-            #func_numlist()
-
+        # sorting alphabet list
         if (var_usr_1 == 2):
             func_alphalist()
         
@@ -196,4 +196,6 @@ while True:
             print('This program was made by: Aparajith, Sai Ganesha, Thijil')
             break
 
-#fun_list()
+        else:
+            print('\nInput a proper value...\n')
+            var_usr_0 = 0 # returns to menu
